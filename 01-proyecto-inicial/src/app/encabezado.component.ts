7c7545f945df4bc957b108d893ca9452ba29b3d9
davedelf <davideliasdelfino@gmail.com>
2025-07-07 16:42:14 -0300

    creación del componente personalizado

---
+++ 01-proyecto-inicial/src/app/encabezado.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encabezado',
+  standalone: true,
+  templateUrl: './encabezado.component.ts',
+})
+export class EncabezadoComponent {}
