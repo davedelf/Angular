701c3745b75977e899958aed93b8366122060c8f
davedelf <davideliasdelfino@gmail.com>
2025-07-07 18:19:02 -0300

    estilo del componente usuario

--- 01-proyecto-inicial/src/app/app.ts
+++ 01-proyecto-inicial/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EncabezadoComponent } from "./encabezado/encabezado.component";
+import { Usuario } from "./usuario/usuario";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EncabezadoComponent],
+  imports: [RouterOutlet, EncabezadoComponent, Usuario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
