6469a6e921315403e5b42da8b5bd2a7a8da71e8c
davedelf <davideliasdelfino@gmail.com>
2025-07-09 23:59:05 -0300

    Usamos la funcion output() para evitar el decorador @Output. Ambos almacenan un EventEmitter, pero ojo, no son signals

--- 01-proyecto-inicial/src/app/usuario/usuario.ts
+++ 01-proyecto-inicial/src/app/usuario/usuario.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  computed,
-  EventEmitter,
-  Input,
-  input,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-usuario',
@@ -16,10 +9,11 @@ import {
 
 //Añadimos cuerpo a la clase del componente
 export class Usuario {
-  @Input({required:true}) id!:string;
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) nombre!: string;
-  @Output() seleccion = new EventEmitter(); //emite una acción
+  /*   @Output() seleccion = new EventEmitter(); //emite una acción */
+  seleccion = output<string>(); //es un reemplazo al uso tradicional del decorador @Output
   get rutaImagen() {
     return this.avatar;
   }
