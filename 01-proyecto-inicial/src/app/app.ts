d53a77d659d8f43bec82cce180277f5092f52246
davedelf <davideliasdelfino@gmail.com>
2025-07-13 02:27:51 -0300

    Mostrar contenido condicional

--- 01-proyecto-inicial/src/app/app.ts
+++ 01-proyecto-inicial/src/app/app.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EncabezadoComponent } from "./encabezado/encabezado.component";
-import { UsuarioComponent } from "./usuario/usuario";
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { UsuarioComponent } from './usuario/usuario';
 import { USUARIOS_FALSOS } from './usuarios-falsos';
-import { Tareas } from "./tareas/tareas";
+import { Tareas } from './tareas/tareas';
 
 @Component({
   selector: 'app-root',
   imports: [EncabezadoComponent, UsuarioComponent, Tareas],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = '01-proyecto-inicial';
-  usuarios=USUARIOS_FALSOS;
-  idUsuarioSeleccionado="u1";
+  usuarios = USUARIOS_FALSOS;
+  idUsuarioSeleccionado!: string;
 
-  get usuarioSeleccionado(){
-    return this.usuarios.find((usuario)=>usuario.id==this.idUsuarioSeleccionado)!;
+  get usuarioSeleccionado() {
+    return this.usuarios.find(
+      (usuario) => usuario.id == this.idUsuarioSeleccionado
+    )!;
   }
-  alSeleccionarUsuario(id:string){
-    this.idUsuarioSeleccionado=id;
+  alSeleccionarUsuario(id: string) {
+    this.idUsuarioSeleccionado = id;
   }
 }
