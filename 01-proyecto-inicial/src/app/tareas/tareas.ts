c0d6122090e1a7b0283671c9f0547dbd3ae8453e
davedelf <davideliasdelfino@gmail.com>
2025-07-14 13:27:39 -0300

    Renderizar componentes tarea segun el id del usuario seleccionado

--- 01-proyecto-inicial/src/app/tareas/tareas.ts
+++ 01-proyecto-inicial/src/app/tareas/tareas.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Tarea } from "../tarea/tarea";
+import { Tarea } from '../tarea/tarea';
 
 @Component({
   selector: 'app-tareas',
@@ -9,5 +9,36 @@ import { Tarea } from "../tarea/tarea";
   styleUrl: './tareas.css',
 })
 export class Tareas {
-  @Input() nombre?: string;
+  @Input({ required: true }) idUsuario!: string;
+  @Input({ required: true }) nombre!: string;
+
+  tareas = [
+    {
+      id: 't1',
+      idUsuario: 'u1',
+      titulo: 'Dominar Angular',
+      resumen:
+        'Apreder todas las características básicas y avanzadas de Angular cómo apicarlas.',
+      expira: '2025-01-01',
+    },
+    {
+      id: 't2',
+      idUsuario: 'u3',
+      titulo: 'Crear el primer prototipo',
+      resumen: 'Crear el primer prototipo del sitio web de la tienda',
+      expira: '2025-11-05',
+    },
+    {
+      id: 't3',
+      idUsuario: 'u3',
+      titulo: 'Preparar la plantilla del carrito',
+      resumen:
+        'Preparar y describir una plantilla de carrito de compras de la tienda online',
+      expira: '2025-05-16',
+    },
+  ];
+
+  get tareasUsuarioSeleccionado() {
+    return this.tareas.filter((tarea) => tarea.idUsuario == this.idUsuario);
+  }
 }
