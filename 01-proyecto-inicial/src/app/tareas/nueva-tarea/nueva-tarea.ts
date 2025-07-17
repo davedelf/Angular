9e7934d3f9449651c16837a9f4a7fd5597e1657e
davedelf <davideliasdelfino@gmail.com>
2025-07-17 16:43:09 -0300

    Directivas y Vinculación Bidireccional

--- 01-proyecto-inicial/src/app/tareas/nueva-tarea/nueva-tarea.ts
+++ 01-proyecto-inicial/src/app/tareas/nueva-tarea/nueva-tarea.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nueva-tarea',
-  imports: [],
+  imports: [FormsModule], //FormsModule para importar las directivas y enlaces para trabajar con formularios y entradas de los inputs. Vinculacióón bidireccional
   templateUrl: './nueva-tarea.html',
   styleUrl: './nueva-tarea.css',
 })
 export class NuevaTarea {
   @Output() cancelar = new EventEmitter<void>();
+  tituloIngresado = '';
+  resumenIngresado="";
+  fechaIngresada="";
   alCancelar() {
     this.cancelar.emit();
   }
