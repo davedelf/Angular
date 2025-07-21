d4c57f5bae2b46400e1b792db8597e533558c387
davedelf <davideliasdelfino@gmail.com>
2025-07-21 13:00:54 -0300

    Modificamos la configuración regional usando la función registerLocaleData que registra datos de localización (idioma, formato de fecha, moneda, etc.), el Token de inyección de dependencias LOCALE_ID y el objeto de datos localeEs, el cual contiene la configuración regional para el idioma español.

--- 01-proyecto-inicial/src/main.ts
+++ 01-proyecto-inicial/src/main.ts
@@ -2,6 +2,23 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { EncabezadoComponent } from './app/encabezado/encabezado.component';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { LOCALE_ID } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeEs, 'es');
+
+bootstrapApplication(App, {
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
+  ],
+}).catch((err) => console.error(err));
+
+/* 
+  Con registerLocaleData le permitimos a Angular que muestre valores en formato regional, como la fecha,
+  moneda, valores, etc. Podemos cambiar el identificador de idioma 'es' por otro idioma que querramos.
+  Esta opción es util para apps multi-idiomas.
+*/
