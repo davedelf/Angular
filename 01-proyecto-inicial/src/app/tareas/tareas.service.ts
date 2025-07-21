7d20391cda5545aeed7ce8323863fc89e9b3cfd1
davedelf <davideliasdelfino@gmail.com>
2025-07-21 13:18:59 -0300

    Servicios - creación de TareasService  para gestionar las oepraciones CRUD de tareas

---
+++ 01-proyecto-inicial/src/app/tareas/tareas.service.ts
@@ -0,0 +1,47 @@
+import { type NuevaTareaInfo } from '../tarea/tarea.model';
+
+class TareasService {
+  private tareas = [
+    {
+      id: 't1',
+      idUsuario: 'u1',
+      titulo: 'Dominar Angular',
+      resumen:
+        'Apreder todas las características básicas y avanzadas de Angular cómo apicarlas.',
+      expira: '2025-01-01',
+    },
+    {
+      id: 't2',
+      idUsuario: 'u3',
+      titulo: 'Crear el primer prototipo',
+      resumen: 'Crear el primer prototipo del sitio web de la tienda',
+      expira: '2025-11-05',
+    },
+    {
+      id: 't3',
+      idUsuario: 'u3',
+      titulo: 'Preparar la plantilla del carrito',
+      resumen:
+        'Preparar y describir una plantilla de carrito de compras de la tienda online',
+      expira: '2025-05-16',
+    },
+  ];
+
+  obtenerTareasDeUsuario(idUsuario: string) {
+    return this.tareas.filter((tarea) => tarea.idUsuario == idUsuario);
+  }
+
+  agregarTarea(infoDeTarea: NuevaTareaInfo, idUsuario:string) {
+    this.tareas.unshift({
+      id: new Date().getTime().toString(),
+      titulo: infoDeTarea.titulo,
+      resumen: infoDeTarea.resumen,
+      expira: infoDeTarea.fecha,
+      idUsuario: idUsuario,
+    });
+  }
+
+  eliminarTarea(id:string){
+     this.tareas = this.tareas.filter((tarea) => tarea.id !== id);
+  }
+}
--- 01-proyecto-inicial/src/app/tareas/tareas.ts
+++ 01-proyecto-inicial/src/app/tareas/tareas.ts
@@ -15,31 +15,6 @@ export class Tareas {
   @Input({ required: true }) nombre!: string;
   estaAgregandoTareaNueva = false;
 
-  tareas = [
-    {
-      id: 't1',
-      idUsuario: 'u1',
-      titulo: 'Dominar Angular',
-      resumen:
-        'Apreder todas las características básicas y avanzadas de Angular cómo apicarlas.',
-      expira: '2025-01-01',
-    },
-    {
-      id: 't2',
-      idUsuario: 'u3',
-      titulo: 'Crear el primer prototipo',
-      resumen: 'Crear el primer prototipo del sitio web de la tienda',
-      expira: '2025-11-05',
-    },
-    {
-      id: 't3',
-      idUsuario: 'u3',
-      titulo: 'Preparar la plantilla del carrito',
-      resumen:
-        'Preparar y describir una plantilla de carrito de compras de la tienda online',
-      expira: '2025-05-16',
-    },
-  ];
 
   get tareasUsuarioSeleccionado() {
     return this.tareas.filter((tarea) => tarea.idUsuario == this.idUsuario);
@@ -58,13 +33,6 @@ export class Tareas {
   }
 
   alAgregarTarea(infoDeTarea: NuevaTareaInfo) {
-    this.tareas.unshift({
-      id: new Date().getTime().toString(),
-      titulo: infoDeTarea.titulo,
-      resumen: infoDeTarea.resumen,
-      expira: infoDeTarea.fecha,
-      idUsuario: this.idUsuario,
-    });
 
     this.estaAgregandoTareaNueva = false;
   }
