91bbbeb08ec53235532214f37905833b67f6fa39
davedelf <davideliasdelfino@gmail.com>
2025-07-22 16:43:01 -0300

    Implementar formato regional para fecha-hora usando el enfoque de módulo

--- 01-proyecto-inicial/src/app/app.module.ts
+++ 01-proyecto-inicial/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
@@ -13,16 +13,20 @@ import { TareasModule } from './tareas/tareas.module';
     AppComponent,
     EncabezadoComponent,
     UsuarioComponent,
-
   ] /* declarations:[] para componentes standalone:false */,
   bootstrap: [AppComponent],
-  imports: [BrowserModule,CompartidaModule,TareasModule],
+  imports: [BrowserModule, CompartidaModule, TareasModule],
+  providers: [{ provider: LOCALE_ID, useValue: 'es' }],
   /* imports:[] para componentes standalone:true, aunque también para otros módulos - ej.: BrowserModule - */
 })
 export class AppModule {}
 /* Cuando trabajamos con módulos no es necesario colocar el pipe o dependencia en tarea - DatePipe - de manera
 explicita en los imports (ya que los eliminamos al ser standalone:false).
 
-BrowserModule ya importa los pipes necesarios, como en este caso el DatePipe.
+BrowserModule ya importa los pipes necesarios, como en este caso el DatePipe. Ojo, solo tiene scope o puede
+ser utilizado en el componente raiz, que en este caso es app. En otros componentes o modulos
+usamos para acceder al datepipe y componentes de formulario ng FormsModule y CommonModule.
+Tener en cuenta el orden jerarquico o ramas del proyecto para saber qué imports son necesarios cuando trabajamos
+con estructuras basadas en módulos.
 
 imports: [BrowserModule, ]*/
--- 01-proyecto-inicial/src/main.ts
+++ 01-proyecto-inicial/src/main.ts
@@ -1,28 +1,9 @@
 import { platformBrowser } from "@angular/platform-browser";
 import { AppModule } from "./app/app.module";
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeEs,'es');
 
 platformBrowser().bootstrapModule(AppModule)
-/* import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-import { EncabezadoComponent } from './app/encabezado/encabezado.component';
-import { registerLocaleData } from '@angular/common';
-import localeEs from '@angular/common/locales/es';
-import { LOCALE_ID } from '@angular/core';
 
-registerLocaleData(localeEs, 'es');
-
-bootstrapApplication(App, {
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: 'es',
-    },
-  ],
-}).catch((err) => console.error(err));
- */
-/* 
-  Con registerLocaleData le permitimos a Angular que muestre valores en formato regional, como la fecha,
-  moneda, valores, etc. Podemos cambiar el identificador de idioma 'es' por otro idioma que querramos.
-  Esta opción es util para apps multi-idiomas.
-*/
