556c429012f895a6196f3a89158ac82364efc3ca
davedelf <davideliasdelfino@gmail.com>
2025-07-24 20:15:43 -0300

    Usamos Signals en el componente ingreso-usuario para manipular y mostrar los datos

--- 03-calculador-de-inversiones/src/app/ingreso-usuario/ingreso-usuario.component.ts
+++ 03-calculador-de-inversiones/src/app/ingreso-usuario/ingreso-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type IngresoInversion } from '../ingreso-inversion.model';
 
@@ -12,17 +12,17 @@ import { type IngresoInversion } from '../ingreso-inversion.model';
 export class IngresoUsuario {
   @Output() calcular = new EventEmitter<IngresoInversion>();
 
-  inversionInicialIngresada = '0';
-  inversionAnualIngresada = '0;';
-  rendimientoEsperadoIngresado = '5';
-  duracionIngresada = '10';
+  inversionInicialIngresada = signal("0");
+  inversionAnualIngresada = signal("0");
+  rendimientoEsperadoIngresado = signal("5");
+  duracionIngresada = signal("10");
 
   alEnviar() {
     this.calcular.emit({
-      inversionInicial: +this.inversionInicialIngresada,
-      duracion: +this.duracionIngresada,
-      rendimientoEsperado: +this.rendimientoEsperadoIngresado,
-      inversionAnual: +this.inversionAnualIngresada,
+      inversionInicial: +this.inversionInicialIngresada(),
+      duracion: +this.duracionIngresada(),
+      rendimientoEsperado: +this.rendimientoEsperadoIngresado(),
+      inversionAnual: +this.inversionAnualIngresada(),
     });
   }
 }
