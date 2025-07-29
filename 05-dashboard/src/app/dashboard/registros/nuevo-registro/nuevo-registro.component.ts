4699b891db4867de1bd51bf10150a49bc64bed03
davedelf <davideliasdelfino@gmail.com>
2025-07-29 15:25:39 -0300

    Proyeccion de contenido con Multiples Ranuras

--- 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
+++ 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BotonComponent } from '../../../compartida/boton/boton.component';
 
 @Component({
   selector: 'app-nuevo-registro',
   standalone: true,
-  imports: [],
+  imports: [BotonComponent],
   templateUrl: './nuevo-registro.component.html',
   styleUrl: './nuevo-registro.component.css'
 })
