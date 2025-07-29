eeabb67882800e39c6cb4a33ea3f764b439c595e
davedelf <davideliasdelfino@gmail.com>
2025-07-29 17:31:44 -0300

    Componentes personalizados con múltiples elementos y proyección de contenido

--- 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
+++ 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BotonComponent } from '../../../compartida/boton/boton.component';
+import { ControlComponent } from "../../../compartida/control/control.component";
 
 @Component({
   selector: 'app-nuevo-registro',
   standalone: true,
-  imports: [BotonComponent],
+  imports: [BotonComponent, ControlComponent],
   templateUrl: './nuevo-registro.component.html',
   styleUrl: './nuevo-registro.component.css'
 })
