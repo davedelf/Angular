cdc8be36ee616515d3bafb71e02920e94c7c2413
davedelf <davideliasdelfino@gmail.com>
2025-07-30 13:26:59 -0300

    Interactuando con elementos host desde dentro de los componentes

--- 05-dashboard/src/app/compartida/control/control.component.ts
+++ 05-dashboard/src/app/compartida/control/control.component.ts
@@ -6,8 +6,12 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
-})
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+  },
+}) /* Esto es similar a colocar el atributo class="control" cuando creamos el componete app-control en nuevo-registro html,
+solo que ésto se lo da por defecto, entonces siempre tendrá ese atributo y no es necesario especificarlo en cada componente */
 export class ControlComponent {
   etiqueta = input.required<string>();
 }
