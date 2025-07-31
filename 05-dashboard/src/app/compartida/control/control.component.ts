902ccd0980c78f810bc6a0ed46546fa6a68e6190
davedelf <davideliasdelfino@gmail.com>
2025-07-31 15:52:27 -0300

    Con @HostListener y @HostBinding podemos vincular eventos y propiedades, pero no es muy usado, solo por motivos retrocompatibles

--- 05-dashboard/src/app/compartida/control/control.component.ts
+++ 05-dashboard/src/app/compartida/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,9 +15,18 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'alClickear()',
   },
 }) /* Esto es similar a colocar el atributo class="control" cuando creamos el componete app-control en nuevo-registro html,
 solo que ésto se lo da por defecto, entonces siempre tendrá ese atributo y no es necesario especificarlo en cada componente */
 export class ControlComponent {
+  /*   @HostListener('click')
+
+  /*   @HostBinding('class') nombreClase =
+    'control'; /* HostBinding agrega nombreClase como una propiedad del host. Es similar a host:{} */
   etiqueta = input.required<string>();
+  alClickear() {
+    console.log('clickeado');
+  }
 }
+
