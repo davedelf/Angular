e987d6c30aab555678accc1645dbf111cb4bc878
davedelf <davideliasdelfino@gmail.com>
2025-08-05 10:54:54 -0300

    Implementar interfaces en el cliclo de vida de los componetnes para evitar errores tipográficos

--- 05-dashboard/src/app/dashboard/estado-servidor/estado-servidor.component.ts
+++ 05-dashboard/src/app/dashboard/estado-servidor/estado-servidor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-estado-servidor',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './estado-servidor.component.html',
   styleUrl: './estado-servidor.component.css',
 })
-export class EstadoServidorComponent {
+export class EstadoServidorComponent implements OnInit {
   estadoActual: 'online' | 'offline' | 'unknown' = 'unknown';
   ngOnInit() {
     setInterval(() => {
