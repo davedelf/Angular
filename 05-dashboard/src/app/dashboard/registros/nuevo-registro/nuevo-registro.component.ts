8394bed17e77139011ccc3ad123f3f76897969d8
davedelf <davideliasdelfino@gmail.com>
2025-08-05 16:21:19 -0300

    Manejo de formularios con variables de plantilla. Con # asignamos una varibale en la vista del componente (plantilla) y de esta forma podemos manipularlo, acceder a su value, etc. Es una alternativa al enlace bidireccional [(ngModel)].

--- 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
+++ 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BotonComponent } from '../../../compartida/boton/boton.component';
-import { ControlComponent } from "../../../compartida/control/control.component";
+import { ControlComponent } from '../../../compartida/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-registro',
   standalone: true,
-  imports: [BotonComponent, ControlComponent],
+  imports: [BotonComponent, ControlComponent, FormsModule],
   templateUrl: './nuevo-registro.component.html',
-  styleUrl: './nuevo-registro.component.css'
+  styleUrl: './nuevo-registro.component.css',
 })
 export class NuevoRegistroComponent {
-
+  alEnviar(elementoTitulo: HTMLInputElement) {
+    const tituloIngresado = elementoTitulo.value;
+    console.log(`TITULO INGRESADO: ${tituloIngresado}`);
+  }
 }
