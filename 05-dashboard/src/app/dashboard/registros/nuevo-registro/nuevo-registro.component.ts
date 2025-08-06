c583184c5584677fe68372a0d0be1f59deeb7346
davedelf <davideliasdelfino@gmail.com>
2025-08-06 16:24:21 -0300

    Formulario con Signals y ViewChild

--- 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
+++ 05-dashboard/src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { BotonComponent } from '../../../compartida/boton/boton.component';
 import { ControlComponent } from '../../../compartida/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,13 +11,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nuevo-registro.component.css',
 })
 export class NuevoRegistroComponent {
-  @ViewChild('miFormulario') miFormulario?: ElementRef<HTMLFormElement>;
+  /*   @ViewChild('miFormulario') private miFormulario?: ElementRef<HTMLFormElement>; */
+  private miFormulario =
+    viewChild.required<ElementRef<HTMLFormElement>>('miFormulario');
 
   alEnviar(titulo: string, textoIngresado: string) {
     console.log(titulo);
     console.log(textoIngresado);
-    this.miFormulario?.nativeElement.reset();
+    this.miFormulario().nativeElement.reset();
   }
 }
 
 /* Usamos nativeElement xq ElementRef es generico y envuelve a HTMLFormElement */
+/* En viewChild podemos usar ? o alternativamente required, pero con eso estamos obligados a que exista el elemento child */
