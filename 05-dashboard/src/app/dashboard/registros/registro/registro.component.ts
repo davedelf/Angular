74a5c3635048e3e6d903a95c41501062d881cde3
davedelf <davideliasdelfino@gmail.com>
2025-08-08 16:42:36 -0300

    Actualizar valor de Signals con update()

--- 05-dashboard/src/app/dashboard/registros/registro/registro.component.ts
+++ 05-dashboard/src/app/dashboard/registros/registro/registro.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { NuevoRegistroComponent } from "../nuevo-registro/nuevo-registro.component";
+import { Component, input, signal } from '@angular/core';
+import { NuevoRegistroComponent } from '../nuevo-registro/nuevo-registro.component';
+import { Registro } from '../registros.model';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
   imports: [NuevoRegistroComponent],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrl: './registro.component.css',
 })
 export class RegistroComponent {
+  informacion = input.required<Registro>();
+  detallesVisibles = signal(false);
 
+  alAlternarDetalles() {
+    this.detallesVisibles.update((eraVisible) => !eraVisible);
+  }
 }
+/* 
+ .update() funciona como .set(). Toma como parámetro el valor anterior del signal y lo actualiza 
+ */
