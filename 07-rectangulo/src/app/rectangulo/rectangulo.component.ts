d04f312a70c202c151ba12dd02b8671364efe39b
davedelf <davideliasdelfino@gmail.com>
2025-08-11 16:27:42 -0300

    Configuramos Modelos Bidireccionales con ModelSignals

--- 07-rectangulo/src/app/rectangulo/rectangulo.component.ts
+++ 07-rectangulo/src/app/rectangulo/rectangulo.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  input,
+  model,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-rectangulo',
@@ -8,24 +15,31 @@ import { Component, EventEmitter, Input, input, Output } from '@angular/core';
   styleUrl: './rectangulo.component.css',
 })
 export class RectanguloComponent {
-  @Input({ required: true }) tamanio!: {
+  /*   @Input({ required: true }) tamanio!: {
     ancho: string;
     alto: string;
   };
   @Output() tamanioChange = new EventEmitter<{
     ancho: string;
     alto: string;
+  }>(); */
+
+  tamanio = model.required<{
+    ancho: string;
+    alto: string;
   }>();
+  /* esto te ahorra tener que crear inputs y otuputs separados, pero es necesario especificar el tipo de dato o estructura. Esto crea un Model
+  Signal, por lo que lo llamamos como un signal desde la plantilla*/
 
   alResetear() {
-    this.tamanioChange.emit({
+    this.tamanio.set({
       ancho: '200',
       alto: '100',
     });
   }
 }
 
-/* Para enladces bidireccionales, existe una "palabra mágica" o convención, que consiste en colocar 
+/* Para enlaces bidireccionales, existe una "palabra mágica" o convención, que consiste en colocar 
 @Input y @Output con el mismo nombre pero con el sufijo Change en el @Output. Angular lo detecta automáticamente
 y es una convención estandarizada entre desarrolladores. Si le colocamos otro sufijo, no funciona.
 Dato util e importante */
