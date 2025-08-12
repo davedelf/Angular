6285ecf1bfd433d07e056d2d609cd72ba021245f
davedelf <davideliasdelfino@gmail.com>
2025-08-12 16:41:59 -0300

    Directivas de atributo para cambiar comportamiento de elementos

--- 08-directivas/src/app/enlace-seguro.directive.ts
+++ 08-directivas/src/app/enlace-seguro.directive.ts
@@ -1,11 +1,23 @@
-import { Directive } from "@angular/core";
+import { Directive } from '@angular/core';
 
 @Directive({
-    selector:'a[appEnlaceSeguro]',
-    standalone:true,
+  selector: 'a[appEnlaceSeguro]',
+  standalone: true,
+  host: {
+    '(click)': 'alConfirmarSalirPagina($event)',
+  },
 })
-export class EnlaceSeguroDirective{
-    constructor(){
-        console.log("La directiva del enlace seguro está activa");
+export class EnlaceSeguroDirective {
+  constructor() {
+    console.log('La directiva del enlace seguro está activa');
+  }
+
+  alConfirmarSalirPagina(event: MouseEvent) {
+    const quiereSalir = window.confirm('¿Quieres salir de la aplicación?');
+    if (quiereSalir === true) {
+      return;
     }
+
+    event.preventDefault();
+  }
 }
